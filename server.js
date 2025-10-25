f903b6dd03ec23ef54287265f02ce8d22a8b201e
Luis Mario Sandoval <luismariosandoval44@gmail.com>
2025-10-25 00:26:16 -0700

    se agrego server con node

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+// Servir archivos estáticos
 app.use(express.static(path.join(__dirname)));
 
-app.get('*', (req, res) => {
+// Ruta catch-all para Express 5 (cambia * por /*)
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
